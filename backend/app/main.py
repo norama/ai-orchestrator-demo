51460d1f96a05a64d69e037c4e9452049c5ba35a
norama <norama.matema@gmail.com>
2026-01-17 01:16:18 +0100

    Day 9: FastAPI allow "X-Workspace-Id" header

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,7 +37,10 @@ app.add_middleware(
     allow_origins=settings.cors_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=[
+        "Content-Type",
+        "X-Workspace-Id",
+    ],
 )
 
 app.add_exception_handler(
